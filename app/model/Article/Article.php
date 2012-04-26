14dbee9bb7e858680952ebfbbab8f183ff9eec83
Vasek Purchart <me@vasekpurchart.cz>
2012-04-27 01:56:39 +0200

    Article entity

---
+++ app/model/Article/Article.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace MyBlog;
+
+use DateTime;
+
+/**
+ * @Entity
+ */
+class Article extends \MyBlog\Entity
+{
+
+	/**
+	 * @var string
+	 *
+	 * @Column(length=100)
+	 */
+	private $title;
+
+	/**
+	 * @var DateTime
+	 *
+	 * @Column(type="date")
+	 */
+	private $date;
+
+	/**
+	 * @var string
+	 *
+	 * @Column(type="text")
+	 */
+	private $content;
+
+	function __construct()
+	{
+		$this->date = new DateTime();
+	}
+
+	public function getTitle()
+	{
+		return $this->title;
+	}
+
+	public function setTitle($title)
+	{
+		$this->title = $title;
+	}
+
+	public function getContent()
+	{
+		return $this->content;
+	}
+
+	public function setContent($content)
+	{
+		$this->content = $content;
+	}
+
+	public function getDate()
+	{
+		return $this->date;
+	}
+
+}
+
