f9e75c437cfc367f74ccb5862e0bf72c9b592e9a
Vasek Purchart <me@vasekpurchart.cz>
2012-04-27 03:30:09 +0200

    front - list articles

--- app/FrontModule/presenters/ArticlePresenter.php
+++ app/FrontModule/presenters/ArticlePresenter.php
@@ -2,12 +2,24 @@
 
 namespace FrontModule;
 
+use Doctrine\ORM\EntityManager;
+
 class ArticlePresenter extends \FrontModule\BasePresenter
 {
 
+	/**
+	 * @var Doctrine\ORM\EntityManager
+	 */
+	private $entityManager;
+
+	function __construct(EntityManager $entityManager)
+	{
+		$this->entityManager = $entityManager;
+	}
+
 	public function renderDefault()
 	{
-
+		$this->template->articles = $this->entityManager->getRepository('MyBlog\Article')->findAll();
 	}
 
 }
